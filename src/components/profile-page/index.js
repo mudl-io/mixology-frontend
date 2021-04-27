5b7f6e47ec6311d073bf4e74bf261fa61a5e8176
oFrusch <owenc343@gmail.com>
2021-04-26 22:45:48 -0400

    created image upload modal component and added logic to create and retrieve profile pictures

--- src/components/profile-page/index.js
+++ src/components/profile-page/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import { NotificationManager } from "react-notifications";
 
 import "./styles.scss";
 import axiosInstance from "../../axiosApi";
+import defaultProfilePic from "../../assets/cocktail-silhouette.png";
+import ImageUploadModal from "../image-upload-modal";
 
 class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      username: "",
-      email: "",
-      savedCocktailsCount: 0,
+      activeProfilePicture: null,
       createdCocktailsCount: 0,
+      email: "",
+      imageSelected: false,
+      profilePictureToUpload: null,
+      savedCocktailsCount: 0,
+      showUploader: false,
+      username: "",
       viewedCocktailsCount: 0,
     };
   }
@@ -26,10 +34,11 @@ class ProfilePage extends React.Component {
       });
 
       this.setState({
-        username: response.data.username,
-        email: response.data.email,
-        savedCocktailsCount: response.data.savedCocktailsCount,
         createdCocktailsCount: response.data.createdCocktailsCount,
+        email: response.data.email,
+        activeProfilePicture: response.data.activeProfilePicture.image,
+        savedCocktailsCount: response.data.savedCocktailsCount,
+        username: response.data.username,
         viewedCocktailsCount: response.data.viewedCocktailsCount,
       });
     } catch (e) {
@@ -41,10 +50,64 @@ class ProfilePage extends React.Component {
     }
   }
 
+  handleSaveProfilePicture = async () => {
+    if (this.state.profilePictureToUpload) {
+      const imageData = new FormData();
+      imageData.append("image", this.state.profilePictureToUpload);
+      axiosInstance.defaults.headers["Content-Type"] = "multipart/form-data";
+
+      try {
+        const res = await axiosInstance.post("/profile_pictures/", imageData);
+
+        this.setState({
+          activeProfilePicture: res.data.image,
+        });
+
+        this.toggleShowUploader();
+
+        NotificationManager.success(
+          "Profile picture successfully uploaded",
+          "Upload Success",
+          2000
+        );
+      } catch (e) {
+        NotificationManager.error(
+          "Error uploading profile picture. Please try again or refresh the page",
+          "Upload failure",
+          2000
+        );
+      } finally {
+        axiosInstance.defaults.headers["Content-Type"] = "application/json";
+      }
+    } else {
+      NotificationManager.error("No image selected", "Upload failure", 2000);
+    }
+  };
+
+  handleUploadProfilePicture = (profilePicture) => {
+    this.setState({
+      profilePictureToUpload: profilePicture[0],
+      imageSelected: true,
+    });
+  };
+
+  toggleShowUploader = () => {
+    this.setState({ showUploader: !this.state.showUploader });
+  };
+
   render() {
     return (
       <div className="profile-page">
         <div className="inner-content">
+          <div className="profile-image-and-uploader">
+            <img
+              className="profile-picture"
+              src={this.state.activeProfilePicture || defaultProfilePic}
+            />
+            <div className="upload-icon">
+              <AddAPhotoIcon onClick={this.toggleShowUploader} />
+            </div>
+          </div>
           <div className="username">
             <h3>Username:</h3>
             <div>{this.state.username}</div>
@@ -70,6 +133,15 @@ class ProfilePage extends React.Component {
             </div>
           </div>
         </div>
+
+        <ImageUploadModal
+          buttonText={"Upload a new profile picture"}
+          imageSelected={this.state.imageSelected}
+          open={this.state.showUploader}
+          handleClose={this.toggleShowUploader}
+          uploadImage={this.handleUploadProfilePicture}
+          saveImage={this.handleSaveProfilePicture}
+        />
       </div>
     );
   }
